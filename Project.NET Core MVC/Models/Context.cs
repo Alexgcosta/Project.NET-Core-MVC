9e1bebd67adb15399c2d85126111d4ec8ad80cea
Alexgcosta <alexgcosta81@gmail.com>
2021-10-29 02:37:35 -0300

    Adicionar arquivos de projeto.

---
+++ Project.NET Core MVC/Models/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Project.NET_Core_MVC.Models
+{
+    public class Context : DbContext
+    {
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+        {
+            OptionsBuilder.UseSqlServer( @"Server=(localdb)\mssqlLocalDb;Database=Project.NET Core MVC;Integrated Security=True");
+        }
+    }
+}
+
